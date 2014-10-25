6a40ac64d5fca7a3a5a300c521352e2dc7d9f43e
Lukas <luk.prokop@gmail.com>
2014-10-25 12:50:53 +0200

    Switching depends on temperature

--- gpio.py
+++ gpio.py
@@ -5,9 +5,16 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.OUT)
 
-while True:
-	GPIO.output(18, True)
-	time.sleep(5)
-	GPIO.output(18, False)
-	time.sleep(5)
+def readTemp(sensor):
+	f = open(sensor, "r")
+	temp = f.read().strip()
+	return temp
 
+temp = float(readTemp("/mnt/1wire/28.365327060000/temperature12"))
+print(temp)
+if temp > 30:
+	GPIO.output(18, False)
+else:
+	GPIO.output(18, True)
+
+#GPIO.cleanup()
